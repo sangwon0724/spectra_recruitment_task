443a4e4f775e2b1a89370a5f6a3380c1545fa227
Sangwon <65121442+sangwon0724@users.noreply.github.com>
2025-06-05 19:21:59 +0900

    init

---
+++ src/test/java/spectra/recruitment/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package spectra.recruitment.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
