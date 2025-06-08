af525970e6e91229a54d7c8af042163fbf0edc47
Sangwon <65121442+sangwon0724@users.noreply.github.com>
2025-06-08 23:43:33 +0900

    로그인에 대한 유효성 검증 보강

--- src/main/java/spectra/recruitment/task/dto/LoginDto.java
+++ src/main/java/spectra/recruitment/task/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package spectra.recruitment.task.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginDto {
+    @NotEmpty(message ="필요한 데이터 중 [아이디]에 대한 값이 존재하지 않습니다.")
     private String loginId;
+    @NotEmpty(message ="필요한 데이터 중 [비밀번호]에 대한 값이 존재하지 않습니다.")
     private String loginPw;
 }
